14b4a45d931ad1e7d958cd8cea4d06e27531a773
Jurgen De Jonghe <jurgen@myteam.be>
2020-02-24 22:34:01 +0100

    First working version (code from archives)

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace RenameToFoldername
 {
@@ -10,6 +12,68 @@ namespace RenameToFoldername
     {
         static void Main(string[] args)
         {
+            if (args.Count() != 1)
+            {
+                showHelptext();
+            }
+            else if (args[0].ToLower() == "test")
+            {
+                Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly().CodeBase);
+                Console.WriteLine(System.AppDomain.CurrentDomain.BaseDirectory);
+                Console.WriteLine(System.AppDomain.CurrentDomain.FriendlyName);
+            }
+            else if (args[0].ToLower() == "+register")
+            {
+                Console.WriteLine("registering application in Windows Explorer Context Menu");
+
+                string menuCommand = string.Format("\"{0}\" \"%L\"", System.AppDomain.CurrentDomain.BaseDirectory + System.AppDomain.CurrentDomain.FriendlyName);
+                FileShellExtension.Register("*", "Rename to foldername",
+                                            "Rename to foldername", menuCommand);
+            }
+            else if (args[0].ToLower() == "-register")
+            {
+                Console.WriteLine("unregistering application from Windows Explorer Context Menu");
+                FileShellExtension.Unregister("*", "Rename to foldername");
+            }
+            else
+            {
+                renameFileToFoldername(args[0]);
+            }
+        }
+
+        static void renameFileToFoldername(string filename)
+        {
+            FileInfo fi = new FileInfo(filename);
+            if (fi.Exists)
+            {
+                DirectoryInfo di = fi.Directory;
+                string newName = di.FullName + "\\" + di.Name + fi.Extension;
+
+                if (!File.Exists(newName))
+                {
+                    File.Move(fi.FullName, newName);
+                }
+                else
+                {
+                    MessageBox.Show("file already exists (" + newName + ")");
+                }
+            }
+            else
+            {
+                Console.WriteLine("file does noet exist");
+                Console.WriteLine("");
+                showHelptext();
+            }
+        }
+
+        static void showHelptext()
+        {
+            Console.WriteLine("arguments:  filename / +register / -register");
+            Console.WriteLine("");
+            Console.WriteLine("filename :  file to rename based on folder name");
+            Console.WriteLine("+register : register in file explorer context menu");
+            Console.WriteLine("-register : unregister from file explorer context menu");
+            Console.WriteLine("");
         }
     }
 }
